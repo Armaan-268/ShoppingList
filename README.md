916adeff017928a73ec7b7a2d0e847feed1c6ed8
ARMAAN CHAUHAN <armaanchauhan268@gmail.com>
2023-06-13 22:52:46 +0530

    First Build

---
+++ README.md
@@ -0,0 +1,40 @@
+# ShoppingList
+
+ShoppingList is a web application that allows users to create and manage their shopping lists. It is built using HTML, CSS, and JavaScript.
+
+## Features
+
+- **Add items**: Users can easily add items to their shopping list by typing the item name and clicking the "Add" button or pressing Enter key. Double click item to move it to Discarded list.
+- **Discard items**: Users can remove items from the Discarded list by clicking the "Discard" button at the end.
+- **Responsive design**: The application is designed to be responsive and works well on different screen sizes, including mobile devices.
+
+## Getting Started
+
+To use the ShoppingList application, follow these steps:
+
+1. Clone the repository: `git clone https://github.com/Armaan-268/ShoppingList`
+2. Open the project folder.
+3. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Type the name of the item you want to add to the shopping list in the input field.
+2. Click the "Add" button or press Enter to add the item to the list.
+3. To remove an item from the list, click the "Discard" button next to the item.
+4. To clear the entire shopping list, click the "Clear All" button.
+
+## Contributing
+
+If you want to contribute to this project and make it better, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch: `git checkout -b feature/your-feature`
+3. Make your changes and commit them: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin feature/your-feature`
+5. Submit a pull request.
+
+
+
+## Contact
+
+If you have any questions or suggestions, feel free to contact me at [armaanchauhan268@gmail.com](mailto:armaanchauhan268@gmail.com).
---
+++ index.js
@@ -0,0 +1,249 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-f422a-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+// Defining variables
+let mode = false;
+let lastTapTime = 0;
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+const discardListInDB = ref(database, "discardList")
+
+const delButtonEl = document.getElementById("del-button")
+const discardListEl = document.getElementById("discard-list")
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+const darkModeBtn = document.getElementById("dark")
+const theme = document.getElementById("dlmode")
+
+//Add and Discard Buttons
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    if (inputValue.length > 0) {
+    push(shoppingListInDB, inputValue)}
+    clearInputFieldEl()
+})
+delButtonEl.addEventListener("click", function() {
+    discardListEl.innerHTML = ""
+    let exactLocationOfItemInDB = ref(database, `discardList`)
+    remove(exactLocationOfItemInDB)
+})
+
+// Functions to update Lists
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
+        for (const element of itemsArray) {
+            let currentItem = element
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = `<li class="listItem">No items here...</li>`
+    }
+    if(mode){
+        darkMode()
+    }
+    else{
+        lightMode()
+    }
+})
+onValue(discardListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+        discardListEl.innerHTML = ""
+        
+        for (const element of itemsArray) {
+            let currentItem = element
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToDiscardListEl(currentItem)
+        }    
+    } else {
+        discardListEl.innerHTML = ""
+    }
+    if(mode){
+        darkMode()
+    }
+    else{
+        lightMode()
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+function appendItemToDiscardListEl(item){
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    newEl.setAttribute("class","discardItem")
+    newEl.textContent = itemValue
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `discardList/${itemID}`)
+        push(shoppingListInDB,itemValue)
+        remove(exactLocationOfItemInDB)
+    })
+    newEl.addEventListener("touchend", function(event) {
+        const currentTime = new Date().getTime();
+        const tapTimeDiff = currentTime - lastTapTime;
+      
+        if (tapTimeDiff < 300) {
+            let exactLocationOfItemInDB = ref(database, `discardList/${itemID}`)
+        push(shoppingListInDB,itemValue)
+        remove(exactLocationOfItemInDB)
+          lastTapTime = 0;
+        } else {
+          lastTapTime = currentTime;
+        }
+        event.preventDefault();
+      });
+    discardListEl.append(newEl)
+}
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    newEl.setAttribute("class","listItem")
+    newEl.textContent = itemValue
+    
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        push(discardListInDB,itemValue)
+        remove(exactLocationOfItemInDB)
+    })
+    newEl.addEventListener("touchend", function(event) {
+        const currentTime = new Date().getTime();
+        const tapTimeDiff = currentTime - lastTapTime;
+      
+        if (tapTimeDiff < 300) {
+            let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+            push(discardListInDB,itemValue)
+            remove(exactLocationOfItemInDB)
+          lastTapTime = 0;
+        } else {
+          lastTapTime = currentTime;
+        }
+      
+        event.preventDefault();
+      });
+    
+    shoppingListEl.append(newEl)
+}
+
+// Functions to change between light and dark modes
+function darkMode(){
+    mode = true
+    let allDivs = document.querySelectorAll('div');
+    let body = document.querySelectorAll('body');
+    let image = document.getElementById('imgLogo');
+    let dmode = document.getElementById("dlmode");
+    let dkbtn = document.getElementById("dark");
+    let iput = document.getElementById("input-field");
+    let lis = document.getElementsByClassName('listItem');
+    let dlis = document.getElementsByClassName('discardItem');
+    let adbtn = document.getElementById('add-button');
+    let dlbtn = document.getElementById('del-button');
+    
+    adbtn.style['background-color'] = '#FFECC7';
+    dlbtn.style['background-color'] = '#FFECC7';
+    adbtn.style['color'] = '#5c3d2e';
+    dlbtn.style['color'] = '#5c3d2e';
+    iput.style['background-color'] = '#5c3d2e';
+    iput.style['color'] = '#FFECC7';
+    dkbtn.style['background-color'] = '#5c3d2e';
+    dkbtn.style['color'] = '#FFECC7';
+    dmode.textContent = "light_mode"
+    image.setAttribute("src","Dark.png")
+    body[0].style['background-color'] = '#2d2424';
+
+    for(const element of allDivs){
+    element.style['background-color'] = '#2d2424';
+    element.style['color'] = '#5c3d2e';
+    }
+    for(const element of lis){
+        element.style['background-color'] = '#5c3d2e';
+        element.style['color'] = '#FFECC7';
+        }
+    for(const element of dlis){
+        element.style['background-color'] = '#3b271d';
+        element.style['color'] = '#FFECC7';
+        }
+}
+function lightMode(){
+    mode = false
+    let allDivs = document.querySelectorAll('div');
+    let body = document.querySelectorAll('body');
+    let image = document.getElementById('imgLogo');
+    let dmode = document.getElementById("dlmode");
+    let dkbtn = document.getElementById("dark");
+    let iput = document.getElementById("input-field")
+    let lis = document.getElementsByClassName('listItem');
+    let dlis = document.getElementsByClassName('discardItem');
+    let adbtn = document.getElementById('add-button');
+    let dlbtn = document.getElementById('del-button');
+    
+    adbtn.style['background-color'] = '#5c3d2e';
+    dlbtn.style['background-color'] = '#5c3d2e';
+    adbtn.style['color'] = '#FFECC7';
+    dlbtn.style['color'] = '#FFECC7';
+    iput.style['background-color'] = '#ffffff';
+    iput.style['color'] = '#5c3d2e';
+    dkbtn.style['background-color'] = '#FFECC7';
+    dkbtn.style['color'] = '#5c3d2e';
+    dmode.textContent = "dark_mode"
+    body[0].style['background-color'] = '#F9FBE7';
+    image.setAttribute("src","Light.png")
+
+    for(const element of allDivs){
+    element.style['background-color'] = '#F9FBE7';
+    element.style['color'] = 'black';
+    }
+    for(const element of lis){
+        element.style['background-color'] = '#FFFDFF';
+        element.style['color'] = '#5c3d2e';
+        }
+    for(const element of dlis){
+        element.style['background-color'] = '#e0c097';
+        element.style['color'] = '#5c3d2e';
+        }
+}
+
+// Dark Mode Button event
+darkModeBtn.addEventListener("click",function(){
+    if (!mode){
+        darkMode();
+    }
+    else{
+        lightMode();
+    }
+})
+// Input field ENTER key detection
+inputFieldEl.addEventListener("keypress", function(event) {
+    const keyCode = event.keyCode || event.which;
+    if (keyCode === 13) {
+        let inputValue = inputFieldEl.value
+        if (inputValue.length > 0) {
+        push(shoppingListInDB, inputValue)}
+        clearInputFieldEl()
+    }
+  });
+
+ 
